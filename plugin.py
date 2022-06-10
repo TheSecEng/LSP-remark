364deb88306dca147249fafa548715769e2ea64a
TheSecEng <zachary.schulze@gmail.com>
2022-06-09 20:22:25 -0500

    initial commit

--- plugin.py
+++ plugin.py
@@ -4,16 +4,16 @@ import os
 from lsp_utils import NpmClientHandler
 
 
+class LspRemark(NpmClientHandler):
+    package_name = __package__
+    server_directory = 'server'
+    server_binary_path = os.path.join(
+        server_directory, 'node_modules', 'remark-language-server', 'index.js')
+
+
 def plugin_loaded():
     LspRemark.setup()
 
 
 def plugin_unloaded():
     LspRemark.cleanup()
-
-
-class LspRemark(NpmClientHandler):
-    package_name = __package__
-    server_directory = 'server'
-    server_binary_path = os.path.join(
-        server_directory, 'node_modules', 'remark-language-server', 'bin', 'remark-language-server')
